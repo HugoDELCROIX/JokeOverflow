bf4bcfd35ecb8ea1f2b394ee6043cbc881a94a7c
Rhylionn <tbernard.p@protonmail.com>
2022-04-17 21:35:39 +0200

    added bottom navigation bar + fragments

--- app/src/main/java/com/example/jokeoverflow/MainActivity.java
+++ app/src/main/java/com/example/jokeoverflow/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.jokeoverflow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        final NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.homeFragment){
+                    navController.navigate(R.id.homeFragment);
+                } else if(item.getItemId() == R.id.leaderboardFragment){
+                    navController.navigate(R.id.leaderboardFragment);
+                } else if(item.getItemId() == R.id.bestjokesFragment){
+                    navController.navigate(R.id.bestjokesFragment);
+                }
+
+                return true;
+            }
+        });
+
     }
+
 }

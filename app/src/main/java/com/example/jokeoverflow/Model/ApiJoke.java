cf958b588c688b29b9034a14ff4b0afd89f9a101
Constantin-Hentgen <constantin.hentgen@protonmail.com>
2022-05-12 23:12:51 +0200

    add apiJoke model

---
+++ app/src/main/java/com/example/jokeoverflow/Model/ApiJoke.java
@@ -0,0 +1,13 @@
+package com.example.jokeoverflow.Model;
+
+public class ApiJoke {
+    private String joke;
+
+    public String getJoke() {
+        return joke;
+    }
+
+    public void setJoke(String joke) {
+        this.joke = joke;
+    }
+}
